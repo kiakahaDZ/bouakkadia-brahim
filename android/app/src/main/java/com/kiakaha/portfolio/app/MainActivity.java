bfa9af1a8f38aaadcffd6832bad3982009f39b7a
KIAKAHA <fennecsoft23@gmail.com>
2024-03-15 21:22:18 +0100

    feat: add capcitor for android build

---
+++ android/app/src/main/java/com/kiakaha/portfolio/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.kiakaha.portfolio.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kiakaha.portfolio.app',
+  appName: 'fennecSoftPortfolio',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
